6d77dd14032881d56e0e3bd920ca20fffaa319fc
George Makura <makurag@proton.me>
2023-10-09 16:58:35 +0200

    Add 2 error raise

--- 01_py/guess.py
+++ 01_py/guess.py
@@ -1,16 +1,29 @@
 import random
 
-randomne_cislo = random.randint(1, 100)
+MIN_VALUE = 1
+MAX_VALUE = 100
+
+def safe_input():
+    while True:
+        try:
+            user_tip = int(input("Tvůj tip: "))
+        except:
+            print("Není to číslo! ")
+        return user_tip
+
+randomne_cislo = random.randint(MIN_VALUE, MAX_VALUE)
 
 finished = False
 
 while not finished:
-    tip = int(input("Tvuj tip: "))
-    if tip == randomne_cislo:
-        print("Vyhra!")
-        finished = True
-    elif randomne_cislo > tip:
-        print ("Moc male")
-    elif randomne_cislo < tip:
-        print ("Moc velke")
-    
+    tip = safe_input()
+    if tip <= MAX_VALUE and tip >= MIN_VALUE:
+        if tip == randomne_cislo:
+            print("Vyhra!")
+            finished = True
+        elif randomne_cislo > tip:
+            print ("Moc male")
+        elif randomne_cislo < tip:
+            print ("Moc velke")
+    else:
+        raise ValueError("Číslo mimo zadany rozsah")
