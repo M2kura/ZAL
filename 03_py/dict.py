1379ff94d2d3d60106280b9cc58ba0c64c393394
George Makura <makurag@proton.me>
2023-10-09 17:37:55 +0200

    make dict.py

---
+++ 03_py/dict.py
@@ -0,0 +1,11 @@
+categories = {
+    "apple": "jablko",
+    "pear": "hruška",
+    "nut": "ořech"
+}
+
+for key in categories:
+    print(key, categories[key])
+
+for i in categories:
+    print(i)
