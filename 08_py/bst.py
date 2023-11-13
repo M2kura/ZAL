13ecb4308d510d3763f54b1c71abb7122d74ba79
George Makura <makurag@proton.me>
2023-11-13 04:24:39 +0100

    min stable version

--- 08_py/bst.py
+++ 08_py/bst.py
@@ -59,7 +59,17 @@ class BinarySearchTree:
                 return False
 
     def min(self):
-        pass
+        count = 0
+        current = self.head
+        if not self.head:
+            return None
+        while current:
+            if not current.left:
+                count += 1
+                self.visited = count
+                return current.value
+            current = current.left
+            count += 1
 
     def max(self):
         pass
@@ -69,3 +79,5 @@ class BinarySearchTree:
 
 bst2 = BinarySearchTree()
 bst2.fromArray([5, 3, 5, 1, 4, 7, 6, 8])
+print(bst2.min())
+print(bst2.visitedNodes())
